a61ce5c7b55b91b05449db44acbf6578b0948c23
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-11-09 17:35:42 +0530

    Configured directories

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Frugal</h1>
+      <h1>Budgex</h1>
       <h2>Personal Finance Tracker</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
